de58ba984564313f04a6af5cb74637215e308363
WADE <MEANTOINE12@GMAIL.COM>
2025-04-11 23:06:48 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import Article from "./Article";
+import Header from "./Header";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+
+      <Header name = {blogData.name}/>
+      <About about={blogData.about}/>
+      <ArticleList posts={blogData.posts}/>
+      
     </div>
   );
 }
---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Article ({title,preview,date="January 1, 1970"}){
+    return(
+        <article>
+            <h3>{title}</h3>
+            <small>{date}</small>
+            <p>{preview}</p>
+
+        </article>
+    )
+}
+
+export default Article
